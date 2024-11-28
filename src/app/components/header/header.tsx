7e07d8b9528ce653da1e406b4896a65d7ae10a20
Grant Stevens <grantstevens22@gmail.com>
2024-11-28 15:41:52 +0000

    tidy

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,11 +1,9 @@
 import Profile from "../profile/profile";
-import Image from "next/image";
 import s from "./header.module.scss";
 
 const Header = async () => {
   return (
     <div className={s.header}>
-      {/* <Image src={"/logo.png"} alt={"logo"} height={30} width={30} /> */}
       <h2 className={s.title}>To-do list</h2>
       <Profile />
     </div>
