1280aaa4b51b70bcd2ff9609bbbdb8ee30afd147
Grant Stevens <grantstevens22@gmail.com>
2024-11-29 16:51:36 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
